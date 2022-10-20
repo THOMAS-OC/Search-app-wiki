11b65a3390cc8612afcaab213a5befbda3ccb597
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-10-20 12:00:32 +0200

    Projet en cours de finition

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const form = document.querySelector("form")
 let input = ""
+const section = document.querySelector("section")
+let results = 
 form.addEventListener("submit", (event) => {
+
+    section.innerHTML = ""
 
     event.preventDefault()
     input = document.querySelector("input").value
 
     fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=10&srsearch=${input}`)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+        
+        results = data["query"]["search"]
+        console.log(results)
+
+        for (const result of results) {
+            console.log(result);
+            let newArticle = document.createElement("article");
+            newArticle.innerHTML = `<a target="_blank" href="https://en.wikipedia.org?curid=${result["pageid"]}"> ${result["title"]} </a> <p> https://en.wikipedia.org?curid=${result["pageid"]} </p> <p> ${result["snippet"]} </p> `
+            section.appendChild(newArticle)
+            
+
+        }
+    
+    });
+
+    
     
 })
+
+// https://en.wikipedia.org?curid=${}
